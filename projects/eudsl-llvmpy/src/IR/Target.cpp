869d96fea9aad9059bae496a2794638493e1b032
makslevental <maksim.levental@gmail.com>
2026-08-05 12:31:21 -0700

    [eudsl-llvmpy] Bind Target, TargetMachine, DataLayout, asm and object emission

---
+++ projects/eudsl-llvmpy/src/IR/Target.cpp
@@ -0,0 +1,81 @@
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "IR/Common.h"
+#include "IR/Ownership.h"
+
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/MC/TargetRegistry.h>
+#include <llvm/Support/CodeGen.h>
+#include <llvm/Target/TargetMachine.h>
+#include <llvm/Target/TargetOptions.h>
+#include <llvm/TargetParser/Host.h>
+#include <llvm/TargetParser/Triple.h>
+
+#include <memory>
+#include <optional>
+
+namespace {
+struct TM {
+  std::unique_ptr<llvm::TargetMachine> tm;
+};
+
+std::string emit(TM &self, eudsl::Module &mod, llvm::CodeGenFileType type) {
+  std::string buf;
+  llvm::raw_string_ostream os(buf);
+  llvm::buffer_ostream bos(os);
+  llvm::legacy::PassManager pm;
+  if (self.tm->addPassesToEmitFile(pm, bos, nullptr, type))
+    throw std::runtime_error("target cannot emit this file type");
+  pm.run(mod.get());
+  return buf;
+}
+} // namespace
+
+void populate_target(nb::module_ &m) {
+  m.def("host_triple", []() { return llvm::sys::getDefaultTargetTriple(); });
+
+  nb::class_<TM>(m, "TargetMachine")
+      .def(
+          "__init__",
+          [](TM *self, const std::string &triple, const std::string &cpu,
+             const std::string &features) {
+            std::string tripleStr =
+                triple.empty() ? llvm::sys::getDefaultTargetTriple() : triple;
+            llvm::Triple tt(tripleStr);
+            std::string err;
+            const llvm::Target *target =
+                llvm::TargetRegistry::lookupTarget(tt, err);
+            if (!target)
+              throw std::runtime_error(err);
+            llvm::TargetOptions opts;
+            llvm::TargetMachine *tm = target->createTargetMachine(
+                tt, cpu, features, opts, std::nullopt);
+            if (!tm)
+              throw std::runtime_error("could not create TargetMachine for " +
+                                       tripleStr);
+            new (self) TM{std::unique_ptr<llvm::TargetMachine>(tm)};
+          },
+          "triple"_a = "", "cpu"_a = "", "features"_a = "")
+      .def_prop_ro("triple",
+                   [](TM &self) { return self.tm->getTargetTriple().str(); })
+      .def_prop_ro("data_layout_str",
+                   [](TM &self) {
+                     return self.tm->createDataLayout()
+                         .getStringRepresentation();
+                   })
+      .def(
+          "emit_assembly",
+          [](TM &self, eudsl::Module &mod) {
+            return emit(self, mod, llvm::CodeGenFileType::AssemblyFile);
+          },
+          "module"_a)
+      .def(
+          "emit_object",
+          [](TM &self, eudsl::Module &mod) {
+            std::string obj = emit(self, mod, llvm::CodeGenFileType::ObjectFile);
+            return nb::bytes(obj.data(), obj.size());
+          },
+          "module"_a);
+}
