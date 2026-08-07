a61dc61b98d6560892f949202787ef654d67e727
makslevental <maksim.levental@gmail.com>
2026-08-07 10:38:26 -0700

    [eudsl-llvmpy] Adopt the llvm.types submodule in tests

--- projects/eudsl-llvmpy/tests/test_constants.py
+++ projects/eudsl-llvmpy/tests/test_constants.py
@@ -7,11 +7,11 @@ from llvm.testing import assert_no_leaks
 
 def test_const_int():
     with llvm.Context() as ctx:
-        c = llvm.const_int(llvm.i32(ctx), 42)
+        c = llvm.const_int(llvm.types.i32(ctx), 42)
         assert type(c).__name__ == "ConstantInt"
         assert c.value == 42
         assert str(c) == "i32 42"
-        neg = llvm.const_int(llvm.i32(ctx), -1, signed=True)
+        neg = llvm.const_int(llvm.types.i32(ctx), -1, signed=True)
         assert neg.value == -1
     assert_no_leaks()
 
@@ -21,7 +21,7 @@ def test_const_bool_and_fp():
         t = llvm.const_bool(ctx, True)
         assert type(t).__name__ == "ConstantInt"
         assert str(t) == "i1 true"
-        f = llvm.const_fp(llvm.f64(ctx), 1.5)
+        f = llvm.const_fp(llvm.types.f64(ctx), 1.5)
         assert type(f).__name__ == "ConstantFP"
         assert f.double_value == 1.5
         assert str(f) == "double 1.500000e+00"
@@ -30,8 +30,8 @@ def test_const_bool_and_fp():
 
 def test_undef_poison_null():
     with llvm.Context() as ctx:
-        assert type(llvm.undef(llvm.i32(ctx))).__name__ == "UndefValue"
-        assert type(llvm.poison(llvm.i32(ctx))).__name__ == "PoisonValue"
-        assert type(llvm.null(llvm.ptr_t(ctx))).__name__ == "ConstantPointerNull"
-        assert str(llvm.undef(llvm.i32(ctx))) == "i32 undef"
+        assert type(llvm.undef(llvm.types.i32(ctx))).__name__ == "UndefValue"
+        assert type(llvm.poison(llvm.types.i32(ctx))).__name__ == "PoisonValue"
+        assert type(llvm.null(llvm.types.ptr(ctx))).__name__ == "ConstantPointerNull"
+        assert str(llvm.undef(llvm.types.i32(ctx))) == "i32 undef"
     assert_no_leaks()
