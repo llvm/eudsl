299b89b04d4c103e541a1fe1f84810be5d93cf69
makslevental <maksim.levental@gmail.com>
2026-08-09 12:58:57 -0700

    [eudsl-llvmpy] Fix const_int(-1) call in test_elif_elif_three_way to pass signed=True
    Same issue as the classify() test: const_int(i32, -1) with the default
    signed=False now raises ValueError instead of silently reinterpreting -1 as
    a large unsigned value. Pass signed=True explicitly.

--- projects/eudsl-llvmpy/tests/test_dsl_cf.py
+++ projects/eudsl-llvmpy/tests/test_dsl_cf.py
@@ -578,7 +578,7 @@ def test_elif_elif_three_way():
     @canonicalize(using=LLVMCanonicalizer())
     def sign(x: i32) -> i32:
         if x < 0:
-            r = yield llvm.const_int(i32, -1)
+            r = yield llvm.const_int(i32, -1, signed=True)
         elif x.eq(llvm.const_int(i32, 0)):
             r = yield llvm.const_int(i32, 0)
         elif x < 10:
